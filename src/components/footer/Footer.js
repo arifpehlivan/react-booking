2c68c87f12e083f79b6c85f3e93873c84fee30a8
arifpehlivan <arifpehlivanapap@gmail.com>
2022-04-27 23:43:56 +0300

    add footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,53 @@
+import './footer.css'
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <div className="fLists">
+                <div className="fList">
+                    <div className="fListItem">Countries</div>
+                    <div className="fListItem">Regions</div>
+                    <div className="fListItem">Cities</div>
+                    <div className="fListItem">Districts</div>
+                    <div className="fListItem">Airports</div>
+                    <div className="fListItem">Hotels</div>
+                </div>
+                <div className="fList">
+                    <div className="fListItem">Countries</div>
+                    <div className="fListItem">Regions</div>
+                    <div className="fListItem">Cities</div>
+                    <div className="fListItem">Districts</div>
+                    <div className="fListItem">Airports</div>
+                    <div className="fListItem">Hotels</div>
+                </div>
+                <div className="fList">
+                    <div className="fListItem">Countries</div>
+                    <div className="fListItem">Regions</div>
+                    <div className="fListItem">Cities</div>
+                    <div className="fListItem">Districts</div>
+                    <div className="fListItem">Airports</div>
+                    <div className="fListItem">Hotels</div>
+                </div>
+                <div className="fList">
+                    <div className="fListItem">Countries</div>
+                    <div className="fListItem">Regions</div>
+                    <div className="fListItem">Cities</div>
+                    <div className="fListItem">Districts</div>
+                    <div className="fListItem">Airports</div>
+                    <div className="fListItem">Hotels</div>
+                </div>
+                <div className="fList">
+                    <div className="fListItem">Countries</div>
+                    <div className="fListItem">Regions</div>
+                    <div className="fListItem">Cities</div>
+                    <div className="fListItem">Districts</div>
+                    <div className="fListItem">Airports</div>
+                    <div className="fListItem">Hotels</div>
+                </div>
+            </div>
+            <div className="fText">Copyright © 2022 Booking</div>
+        </div>
+    )
+}
+
+export default Footer
